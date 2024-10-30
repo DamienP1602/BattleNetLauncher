97040be55f4a61d23773ad690955fd16fa5ba152
DMystop <jouliogames@gmail.com>
2024-10-30 14:41:31 +0100

    Manager Game

--- BattleNet/BattleNet/ViewModel/FavoriteGamesViewModel.cs
+++ BattleNet/BattleNet/ViewModel/FavoriteGamesViewModel.cs
@@ -21,15 +21,7 @@ namespace BattleNet.ViewModel
 
         public FavoriteGamesViewModel()
         {
-
-            AllGames = new ObservableCollection<Game>()
-            {
-                new Game("Bo6","/Utils/Bo6Icon.png","/Utils/BO6BG.png", "/Utils/BO6Logo.png", "Action Shooter"),
-                new Game("Overwatch2","/Utils/OverIcon.png","/Utils/OWBG.png", "/Utils/OWLogo.png", "Shooter"),
-                new Game("DiabloIV","/Utils/Diablo4Icon.png","/Utils/DiabloBG.png", "/Utils/DiabloLogo.png", "RPG"),
-                new Game("Hearthstone","/Utils/HTIcon.png","/Utils/HTBG.png", "/Utils/HTLogo.png", "RPG"),
-                new Game("Wow","/Utils/WowIcon.png", "/Utils/WowBG.png", "/Utils/WowLogo.png", "MMORPG"),
-            };
+            AllGames = GameManager.games;
             Favorites = new ObservableCollection<Game>();
         }
 
