6b3c96740137acc957554ffb63c5d134403a806b
DMystop <jouliogames@gmail.com>
2024-10-30 13:47:28 +0100

    clean code

--- BattleNet/BattleNet/obj/Debug/net8.0-windows/BattleNet.AssemblyInfo.cs
+++ BattleNet/BattleNet/obj/Debug/net8.0-windows/BattleNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e66841f91859b1f5e300e52dde8d1bcff94a6d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14afdf8fb72d2e22a8c5c0ce58de6011fa3684aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
